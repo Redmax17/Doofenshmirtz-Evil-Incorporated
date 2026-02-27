b442e0f313e52f23c1bfefe7b0d638568bd8f1be
Redmax17 <jamesmc4170@gmail.com>
2026-02-27 15:40:21 -0500

    Changed env names for merge

--- amplify/backend/function/databaseConnection/src/app.js
+++ amplify/backend/function/databaseConnection/src/app.js
@@ -8,7 +8,7 @@ router.get('/health', async (req, res) => {
   try {
     const connection = await mysql.createConnection({
       host: 'doofenshmirtz-evil-inc-db.cnei26s2ov1h.us-east-2.rds.amazonaws.com',
-      user: process.env.DB_USERNAME,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       ssl: { rejectUnauthorized: true }

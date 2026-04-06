074a11f7d9ae488a00b8b2c9df5e517e0562413b
Alock0716 <lockhaam2023@mountunion.edu>
2026-04-06 15:08:49 -0400

    Delete myReactApp directory

--- myReactApp/src/components/ChangePassword.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { updatePassword } from "aws-amplify/auth";
-
-// Define the shape of the form state
-interface PasswordForm {
-    oldPassword: string;
-    newPassword: string;
-}
-

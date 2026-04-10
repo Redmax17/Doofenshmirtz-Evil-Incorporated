4a6d04338c452307ffa1be8e2fa95e3ee22c1926
Redmax17 <91354313+Redmax17@users.noreply.github.com>
2026-04-09 20:42:24 -0400

    connected style pages to Account Settings page

--- src/Pages/Account/main.tsx
+++ src/Pages/Account/main.tsx
@@ -1,6 +1,10 @@
+// How the Account Settings page is initalized from Account.html
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "../../Styles/Dashboard.css";
 import Account from "./Account";
+import { system } from "../../Styles/theme";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const rootElement = document.getElementById("root");
 
@@ -10,6 +14,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <Account />
+    <ChakraProvider value={system}>
+      <Account />
+    </ChakraProvider>
   </React.StrictMode>
 );

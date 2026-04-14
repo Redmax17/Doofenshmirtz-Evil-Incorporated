bbb5ea933dfbd1a826153b7d372b261ea1221655
Redmax17 <91354313+Redmax17@users.noreply.github.com>
2026-04-14 18:58:15 -0400

    Added Amplify Backend To Track User Data (Because I Forgor), All Data Can Now Be Tracked In data/resource.ts. Amplify Already Tracks User Account Data, So We Do Not Need To Define It. Added UserSettings To Schema To Track User Settings.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,22 @@
+/* 
+  This File Defines The Database Used To Store Information Accross The Site
+*/
+
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+// Defines The Database Schema
+// Id Is Made Automaticly, So We Do Not Need To Define It 
 const schema = a.schema({
-  Todo: a
+  // Defines Table For Storing User Settings
+  UserSettings: a
     .model({
-      content: a.string(),
+      overSpendingNotification: a.boolean(),
+      lowBalanceNotification: a.boolean(),
+      largeTransactionNotification: a.boolean(),
+      lowBalanceThreshold: a.integer()
     })
-    .authorization((allow) => [allow.guest()]),
+    // allow.owner Means That Users Can Changes These Values
+    .authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -13,6 +24,8 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    // Uses Cognito user Pools For Auth
+    // Means That Users Can Edit Their Own Information Without An Auth Account
+    defaultAuthorizationMode: 'userPool',
   },
 });
